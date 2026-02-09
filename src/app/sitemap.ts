7a902fbb44c0b128c3028b16042a830a589402c1
Maxwell Chan <maxmaxchan@gmail.com>
2026-02-09 14:20:28 -0500

    update

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -3,25 +3,25 @@ import { MetadataRoute } from 'next'
 export default function sitemap(): MetadataRoute.Sitemap {
     return [
         {
-            url: 'https://www.maxwellchan.io',
+            url: 'https://www.maxwellchan.me',
             lastModified: new Date(),
             changeFrequency: 'yearly',
             priority: 1,
         },
         {
-            url: 'https://www.maxwellchan.io/#about',
+            url: 'https://www.maxwellchan.me/#about',
             lastModified: new Date(),
             changeFrequency: 'monthly',
             priority: 0.8,
         },
         {
-            url: 'https://www.maxwellchan.io/#experience',
+            url: 'https://www.maxwellchan.me/#experience',
             lastModified: new Date(),
             changeFrequency: 'monthly',
             priority: 0.8,
         },
         {
-            url: 'https://www.maxwellchan.io/#projects',
+            url: 'https://www.maxwellchan.me/#projects',
             lastModified: new Date(),
             changeFrequency: 'monthly',
             priority: 0.8,
